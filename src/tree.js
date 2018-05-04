f09cc1d5b848a36ee3a02bb4293cda254b56e938
veetors <veetor.s@gmail.com>
2018-05-04 16:59:08 +0300

    refactor build feeds-tree

--- src/tree.js
+++ src/tree.js
@@ -1,22 +1,24 @@
 import { getElement, getElements } from './generic';
 
 const buildItemTree = (data) => {
-  const title = getElement(data, 'channel > title');
-  const description = getElement(data, 'channel > description');
   const itemTitles = getElements(data, 'item > title');
   const itemLinks = getElements(data, 'item > link');
   const itemDescriptions = getElements(data, 'item > description');
 
-  const children = itemTitles.map((elem, index) =>
+  return itemTitles.map((elem, index) =>
     ({
       itemTitle: elem,
       itemLink: itemLinks[index],
       itemDescription: itemDescriptions[index],
     }));
+};
+
+const buildFeedTree = (data) => {
+  const title = getElement(data, 'channel > title');
+  const description = getElement(data, 'channel > description');
+  const children = buildItemTree(data);
 
   return { title, description, children };
 };
 
-const buildFeedsTree = data => data.map(buildItemTree);
-
-export default buildFeedsTree;
+export default buildFeedTree;
